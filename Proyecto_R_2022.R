2b5e00d1b6ae87f5da82a52c99b38a77230a8b15
doogles8 <darv_3@hotmail.com>
2022-07-24 20:25:01 +0000

    TERCER COMMIT

--- Proyecto_R_2022.R
+++ Proyecto_R_2022.R
@@ -1,4 +1,11 @@
-print('hola mundo')
+#TODO PARA GIT
 install.packages("gitcreds")
 library(gitcreds)
 gitcreds_set()
+
+#CONEXIONES A LA BASE DE DATOS
+install.packages("DBI")
+install.packages("RMySQL")
+
+library(DBI)
+library(RMySQL)
