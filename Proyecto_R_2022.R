232245d086b2bda13a4159c6ea1afae0e7d3b9b7
doogles8 <darv_3@hotmail.com>
2022-07-24 22:51:59 +0000

    cuarto commint

--- Proyecto_R_2022.R
+++ Proyecto_R_2022.R
@@ -1,11 +1,21 @@
 #TODO PARA GIT
-install.packages("gitcreds")
+#install.packages("gitcreds")
 library(gitcreds)
 gitcreds_set()
 
 #CONEXIONES A LA BASE DE DATOS
-install.packages("DBI")
-install.packages("RMySQL")
+#install.packages("DBI")
+#install.packages("RMySQL")
 
 library(DBI)
 library(RMySQL)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "u752080463_Gastos",
+  host = "sql741.main-hosting.eu",
+  username = "u752080463_D",
+  password = "Laika888")
+
+
+dbListTables(MyDataBase)
