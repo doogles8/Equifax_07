41c352e19fbc9a2860c8d4dbc28bf4974fa1bac0
doogles8 <darv_3@hotmail.com>
2022-07-25 08:55:37 +0000

    ocho

--- Proyecto_R_2022.R
+++ Proyecto_R_2022.R
@@ -10,6 +10,13 @@ gitcreds_set()
 library(DBI)
 library(RMySQL)
 
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "u752080463_Gastos",
+  host = "sql741.main-hosting.eu",
+  username = "u752080463_D",
+  password = "Laika888")
+
 
 dbListTables(MyDataBase)
 dbListFields(MyDataBase, 'COMPRAS_FAMILIARES')
